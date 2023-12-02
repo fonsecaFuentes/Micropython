e625352741d2b5297f5e050b367c7c9df6782f50
alfre <alfreddinocenti@gmail.com>
2023-12-02 04:56:01 -0300

    Modify file main

--- proyect/main.py
+++ proyect/main.py
@@ -9,26 +9,32 @@ from dht22 import read_dht22
 sensor_acs712 = read_current()
 sensor_zmpt101b = voltage_end()
 
+
 # Configuración del servidor
 url = 'http://192.168.0.170'
 
-# Contador de ejecuciones
-executions_count = 0
-# Número máximo de ejecuciones antes de detenerse
-max_executions = 3
+# # Contador de ejecuciones
+# executions_count = 0
+# # Número máximo de ejecuciones antes de detenerse
+# max_executions = 3
 
 
 def post():
     global executions_count
     response = None
     try:
+        # Leer los sensores
+        sensor_acs712 = read_current()
+        sensor_zmpt101b = voltage_end()
+        dht22_data = read_dht22()
+
         # Realizar una solicitud POST con las variables
         data = ujson.dumps(
             {
                 'Amperios': str(sensor_acs712),
                 'Voltaje': str(sensor_zmpt101b),
-                'Temperatura': str(read_dht22()['Temperatura']),
-                'Humedad': str(read_dht22()['Humedad'])
+                'Temperatura': str(dht22_data['Temperatura']),
+                'Humedad': str(dht22_data['Humedad'])
             }
         )
         response = urequests.post(url, json=data)
@@ -45,7 +51,7 @@ def post():
         print('---------------------------------')
         tim.deinit()
 
-        executions_count += 1
+        # executions_count += 1
 
         # Detener el temporizador después de un número máximo de ejecuciones
         # if executions_count >= max_executions:
